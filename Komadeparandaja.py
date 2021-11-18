db6fa34a527451fc389744dbe36f66161df6def0
Paltser <94616705+Paltser@users.noreply.github.com>
2021-11-18 15:03:22 +0200

    Komaparandus aparaat

---
+++ Komadeparandaja.py
@@ -0,0 +1,9 @@
+def paranda_komavead(lause):
+    lause = lause.replace("sest",", sest ")
+    lause = lause.replace("et",", et ")
+    lause = lause.replace("aga",", aga ")
+    print(lause)
+
+paranda_komavead("ma läksin koju sest mul oli igav aga seal oli igavam")
+paranda_komavead("siin lauses on komad puudu sest ma ei oska panna aga proge paneb")
+paranda_komavead("ema laks poodi sest piim oli otsa saanud")
